498c694e07f04adb4991fcc5076c1db12a652680
Aymeric <aymeric.gioia@hotmail.fr>
2017-03-20 15:04:32 +0100

    first commit

---
+++ loop/exo1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<?php
+	echo $num = 0;
+	for ($i = 1; $i <= 10; $i++){
+		echo $i;
+	}
+	?>
+</body>
+</html>
