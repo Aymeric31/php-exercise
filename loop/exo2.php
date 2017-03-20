1cd2501706a1ffd3465dceb12da229a12bd98def
Aymeric <aymeric.gioia@hotmail.fr>
2017-03-20 16:49:08 +0100

    ajout exo 2 et 3

---
+++ loop/exo2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<?php 
+	$initial = 0;
+	$fourchette = 69;
+	while($initial <= 20){
+		$result= $initial * $fourchette;
+		echo $result;
+		$initial++;
+		echo (" ");
+
+	}
+
+	?>
+	
+</body>
+</html>
---
+++ loop/exo3.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<?php 
+	$initial = 100;
+	$fourchette = 69;
+	while($initial >= 20){
+		$result= $initial * $fourchette;
+		echo $result;
+		$initial--;
+		echo (" ");
+	}
+	?>
+	
+</body>
+</html>
