36bb4c4d689368db0a93c74fcf1dba9a07a0fc6d
Aymeric <aymeric.gioia@hotmail.fr>
2017-03-21 16:45:54 +0100

    ajout exo3

---
+++ array/exo3.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<?php 
+	include "exo1.php";
+	?>
+
+	<br>
+
+	<?php
+
+	echo array_search("juin", $mois);
+
+	?>
+</body>
+</html>
