3f81a43ddf6cb98e87baa9962a7a388466e72c72
Aymeric <aymeric.gioia@hotmail.fr>
2017-03-21 16:26:10 +0100

    ajout exo 2

---
+++ array/exo2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<?php 
+	include 'exo1.php';
+
+	?>
+	
+	<br>
+	<br>
+	
+	<?php
+
+	echo $mois[2];
+
+	?>
+</body>
+</html>
