31f039269a9b2294d028234ca3ba2d8d9c5b24cc
Aymeric <aymeric.gioia@hotmail.fr>
2017-03-21 16:17:17 +0100

    exo 1 array

---
+++ array/exo1.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<?php 
+	$mois = array("janvier", "février","mars", "avril", "mai", "juin","juillet", "aout","septembre","octobre","novembre", "décembre");
+	var_dump($mois);
+	?>
+</body>
+</html>
