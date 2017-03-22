de683d41c5dc7b62de030ba161c9dd7ac1b841fd
Aymeric <aymeric.gioia@hotmail.fr>
2017-03-22 16:40:20 +0100

    exo1

---
+++ form/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<form action="user.php" method="get">
+		<p>Prenom:
+			<input type="text" name="prenom">
+		</p>
+		<p>Nom:
+			<input type="text" name="nom">
+		</p>
+
+	</form>
+</body>
+</html>
