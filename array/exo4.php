32a69800b853a1e8cbf7f689194a9fa3790f9b11
Aymeric <aymeric.gioia@hotmail.fr>
2017-03-22 09:38:28 +0100

    ajout exo 4

---
+++ array/exo4.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<?php 
+	include "exo1.php";
+	?>
+	<br>
+
+	<?php 
+	$mois[7] = "août";
+	echo $mois[7];
+	?>
+</body>
+</html>
