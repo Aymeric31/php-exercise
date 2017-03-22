77297f4735ad8116f1e3ad19b0955eadb6349999
Aymeric <aymeric.gioia@hotmail.fr>
2017-03-22 10:20:33 +0100

    ajout exo5

---
+++ array/exo5.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<?php 
+	$dpt = array(
+		02 => "Aisne",
+		59 => "Nord",
+		60 => "Oise",
+		62 => "Pas-de-Calais",
+		80 => "Somme",
+		);
+	var_dump($dpt);
+	?>
+</body>
+</html>
