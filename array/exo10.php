8bec73aacec4dc9699ab8d8ffc579a992e9d5d7c
Aymeric <aymeric.gioia@hotmail.fr>
2017-03-22 11:53:33 +0100

    ajout exo 10

---
+++ array/exo10.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<?php 
+	$dpt = array(
+		02 => "Aisne",
+		59 => "Nord",
+		60 => "Oise",
+		62 => "Pas-de-Calais",
+		80 => "Somme",
+		);
+	foreach ($dpt as $key => $value) {
+		echo "Le département $value à le numéro $key . <br>";
+	}
+	?>
+</body>
+</html>
